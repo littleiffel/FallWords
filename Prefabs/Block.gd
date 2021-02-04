89989d3b24a66b9f9c22789f2d7d49dfb851ee0c
Thierry Nicola <thierry.nicola@gmail.com>
2021-02-04 18:36:57 +0100

    Init Commit

---
+++ Prefabs/Block.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+
+
+export var SPEED = 50
+var character
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	character = randi()% 26 + 65
+	$Label.text = (char(character)).to_lower()
+
+func _process(delta):
+	var direction_vector := Vector2(0,1)
+	move_and_collide(direction_vector * delta * SPEED)
+
+
+func check_input(input):
+	if input == character:
+		get_tree().call_group("GameManager", "score_up")
+		$AnimationPlayer.play("Destroy")
+	pass
+
+func remove():
+	queue_free()
---
+++ Scenes/Game.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var block_prefab = preload("res://Prefabs/Block.tscn")
+var score = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	update_score()
+	$Timer.start()
+
+func update_score():
+	$HUD/Score.text = String(score)
+
+func _input(event):
+	if event is InputEventKey && not event.is_echo():
+		var blocks = $Blocks.get_children()
+		for block in blocks:
+			if block.has_method("check_input"):
+				block.check_input(event.scancode)
+
+func spawn_new_block():
+	var block = block_prefab.instance()
+	block.global_position = random_position()
+	$Blocks.add_child(block)
+
+func random_position():
+	randomize()
+	var rect = get_viewport().size
+	var random_position = rand_range(20, rect.x -20)
+	return Vector2(random_position, 0)
+	
+func score_up():
+	score += 1
+	update_score()
+
+func _on_Timer_timeout():
+	spawn_new_block()
